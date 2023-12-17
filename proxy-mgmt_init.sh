62085ddb1d2f1f8244ddea3396753410353182be
root <root@DESKTOP-TVK1SA4.localdomain>
2023-12-17 22:13:54 +0800

    fix err

--- proxy-mgmt_init.sh
+++ proxy-mgmt_init.sh
@@ -4,6 +4,7 @@ cp -f  ./proxy-mgmt_restart.timer /etc/systemd/system/proxy-mgmt_restart.timer
 cp -f  ./proxy-mgmt.service /etc/systemd/system/proxy-mgmt.service
 SCRIPT=$(pwd)/get_fast_ip.sh
 chmod 0777 $SCRIPT
+bash $SCRIPT
 (crontab -l 2>/dev/null; echo "0 */3 * * * $SCRIPT") | crontab -
 crontab -l | sort | uniq | crontab -
 sudo systemctl daemon-reload
