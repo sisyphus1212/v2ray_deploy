727a9944d499ae3daa7170a0f6c8ea12c7e02ddb
liaocj <liaocj@yusur.tech>
2023-12-21 12:11:27 +0800

    fix env err

--- proxy-mgmt_init.sh
+++ proxy-mgmt_init.sh
@@ -7,7 +7,7 @@ cp -f  ./proxy-mgmt.service /etc/systemd/system/proxy-mgmt.service
 SCRIPT=$(pwd)/get_fast_ip.sh
 [ $1 ] && speed=$1 || speed=5
 [ $2 ] && timeout=$2 || timeout=300
-CMD="GET_REMOTE=$GET_REMOTE $SCRIPT --speed ${speed} --timeout ${timeout}"
+CMD="$SCRIPT --speed ${speed} --timeout ${timeout}"
 
 BACKENDSH=/run/v2ray_deploy/cloudflare_fast_ip_backend.sh
 mkdir -p `dirname $BACKENDSH`
@@ -20,6 +20,7 @@ set -a
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 http_proxy=$http_proxy
 https_proxy=$https_proxy
+GET_REMOTE=$GET_REMOTE
 set +a
 bash ${CMD}
 EOF
